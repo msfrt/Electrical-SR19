453a6e7e1458e47bc2c20e653b652c4d26d0a45f
Dave Yonkers <daveyonks@gmail.com>
2019-03-30 20:20:20 -0400

    Writing intial code

--- Teensy Codes/2019/ATCC/ATCC_V1.0/ATCC_V1.0.ino
+++ Teensy Codes/2019/ATCC/ATCC_V1.0/ATCC_V1.0.ino
@@ -10,14 +10,9 @@
 //------------------------------------------------------------------------------
 //  Written by:     Dave Yonkers
 //  Created:        03/04/2019
-//  Modified By:    Dave Yonkers
-//  Last Modified:  03/04/2019 6:45 AM Ireland time ;;;;;;))) cheeky
 //  Version:        1.0
 //  Purpose:        Send some sexts over a couple of twisted wires
 //  Description:    Analog-to-CAN converter code
-//
-//  Last change:    Dave started to write the code.
-//
 //------------------------------------------------------------------------------
 
 
@@ -89,10 +84,12 @@ typedef struct
   uint8_t deviceStatus;
 
   int sensReadVal = 0;
-  int sensCalcVal = 0;
+  int sensAvg     = 0;
   int sensMax     = 0;
   int sensMin     = 0;
   int sensCount   = 0;
+  int outVals[3];
+  
 
 } sensor;
 
@@ -109,8 +106,23 @@ sensor RR_ROTOR_TEMP, LR_ROTOR_TEMP;
 // variables for message counter
 int messageCount100Hz = 0;
 
+// define the pins for each sensor - front ATCC
+#define FR_DAMPER_POS_pin A0
+#define FL_DAMPER_POS_pin A1
+#define TRACK_TEMP_pin A2
+#define SWA_pin A3
+#define RR_BRAKE_PRESSURE_pin A4
+#define RL_BRAKE_PRESSURE_pin A5
+#define FR_ROTOR_TEMP_pin A12
+#define FR_BRAKE_PRESSURE_pin A13
+#define FL_ROTOR_TEMP_pin A14
+#define FL_BRAKE_PRESSURE_pin A15
+#define WATER_TEMP_BETWEEN_RADS_pin A19
 
-
+// set the analog read resolution in bits (10 bits yeild an input 0-1023, etc.)
+// initialize a variable for a calculation of the maximum read value from pins
+const int read_resolution_bits = 10; // <--- modify this one
+      int read_resolution      = 0;
 
 
 
@@ -122,8 +134,14 @@ void setup() {
 
   Serial.begin(9600);
 
-  // set the resolution of the analog input from 0-1023 (10bit) to 0-1891 (13bit)
-  analogReadResolution(10);
+  // set the resolution to rad the inputs pins
+  analogReadResolution(read_resolution_bits);
+
+  // calculate the maximum binary input read value from the read resolution
+  // (used for dynamic sensor conversion accuracy)
+  read_resolution = pow(2, read_resolution_bits) - 1;
+
+
 
   switch (ATCC)
   {
@@ -131,18 +149,18 @@ void setup() {
     // FRONT ATCC
     case 0:
       // I took these from the "pinout" excel doc in the ATCC code folder, so it's probably wrong
-      pinMode(13, OUTPUT); // onboard LED
-      pinMode(A0, INPUT); // FR damper position
-      pinMode(A1, INPUT); // FL damper position
-      pinMode(A2, INPUT); // track temp
-      // pinMode(A3, INPUT); // SWA
-      pinMode(A4, INPUT); // RR brake pressure
-      pinMode(A5, INPUT); // RL brake pressure
-      pinMode(A12, INPUT); // FR rotor temp
-      pinMode(A13, INPUT); // FR brake pressure
-      pinMode(A14, INPUT); // FL rotor temp
-      pinMode(A15, INPUT); // FL brake pressure
-      pinMODE(A19, INPUT); // water temp between rads
+      pinMode(13,                           OUTPUT); // onboard LED
+      pinMode(FR_DAMPER_POS_pin,            INPUT);
+      pinMode(FL_DAMPER_POS_pin,            INPUT);
+      pinMode(TRACK_TEMP_pin,               INPUT);
+      pinMode(SWA_pin,                      INPUT);
+      pinMode(RR_BRAKE_PRESSURE_pin,        INPUT);
+      pinMode(RL_BRAKE_PRESSURE_pin,        INPUT);
+      pinMode(FR_ROTOR_TEMP_pin,            INPUT);
+      pinMode(FR_BRAKE_PRESSURE_pin,        INPUT);
+      pinMode(FL_ROTOR_TEMP_pin,            INPUT);
+      pinMode(FL_BRAKE_PRESSURE_pin,        INPUT);
+      pinMode(WATER_TEMP_BETWEEN_RADS_pin,  INPUT);
 
 
       break;
@@ -194,12 +212,38 @@ static void ANA_READ( int sensGroup )
   switch (ATCC){
 
 
+    // front ATCC
+    sensor FR_DAMPER_POS, FL_DAMPER_POS, TRACK_TEMP, FR_BRAKE_PRESSURE;
+    sensor FL_BRAKE_PRESSURE, RR_BRAKE_PRESSURE, RL_BRAKE_PRESSURE;
+    sensor WATER_TEMP_BETWEEN_RADS, FR_ROTOR_TEMP, FL_ROTOR_TEMP, SWA;
+
+    // rear ATCC
+    sensor RR_DAMPER_POS, RL_DAMPER_POS, RIGHT_RAD_TEMP, LEFT_RAD_TEMP;
+    sensor RR_ROTOR_TEMP, LR_ROTOR_TEMP;
+
+
 
     // front ATCC
     case 0:
       switch ( sensGroup ) {
         case 0:
+
           // read analog pins
+          FR_DAMPER_POS.sensReadVal           = analogRead(FR_DAMPER_POS_pin);
+          FL_DAMPER_POS.sensReadVal           = analogRead(FL_DAMPER_POS_pin);
+          FR_BRAKE_PRESSURE.sensReadVal       = analogRead(FR_BRAKE_PRESSURE_pin);
+          FL_BRAKE_PRESSURE.sensReadVal       = analogRead(FR_BRAKE_PRESSURE_pin);
+          RR_BRAKE_PRESSURE.sensReadVal       = analogRead(RR_BRAKE_PRESSURE_pin);
+          RL_BRAKE_PRESSURE.sensReadVal       = analogRead(RL_BRAKE_PRESSURE_pin);
+          FR_ROTOR_TEMP.sensReadVal           = analogRead(FR_ROTOR_TEMP_pin);
+          FL_ROTOR_TEMP.sensReadVal           = analogRead(FL_ROTOR_TEMP_pin);
+          TRACK_TEMP.sensReadVal              = analogRead(TRACK_TEMP_pin);
+          WATER_TEMP_BETWEEN_RADS.sensReadVal = analogRead(WATER_TEMP_BETWEEN_RADS_pin);
+          SWA.sensReadVal                     = analogRead(SWA_pin);
+
+
+
+
           break;
         break;
       }
@@ -243,6 +287,74 @@ static void ANA_STORE( int sensGroup )
         case 0:
           ANA_READ(sensGroup)
           // look at PDM code to copy min/max storage (if applicable)
+
+          // FR damper pos
+          if      ( FR_DAMPER_POS.sensReadVal < FR_DAMPER_POS.sensMin ){ FR_DAMPER_POS.sensMin = FR_DAMPER_POS.sensReadVal; }
+          else if ( FR_DAMPER_POS.sensReadVal > FR_DAMPER_POS.sensMax ){ FR_DAMPER_POS.sensMax = FR_DAMPER_POS.sensReadVal; }
+          FR_DAMPER_POS.sensAvg += FR_DAMPER_POS.sensReadVal;
+          FR_DAMPER_POS.sensCount++;
+
+          // FL damper pos
+          if      ( FL_DAMPER_POS.sensReadVal < FL_DAMPER_POS.sensMin ){ FL_DAMPER_POS.sensMin = FL_DAMPER_POS.sensReadVal; }
+          else if ( FL_DAMPER_POS.sensReadVal > FL_DAMPER_POS.sensMax ){ FL_DAMPER_POS.sensMax = FL_DAMPER_POS.sensReadVal; }
+          FL_DAMPER_POS.sensAvg += FL_DAMPER_POS.sensReadVal;
+          FL_DAMPER_POS.sensCount++;
+
+          // FR brake pressure
+          if      ( FR_BRAKE_PRESSURE.sensReadVal < FR_BRAKE_PRESSURE.sensMin ){ FR_BRAKE_PRESSURE.sensMin = FR_BRAKE_PRESSURE.sensReadVal; }
+          else if ( FR_BRAKE_PRESSURE.sensReadVal > FR_BRAKE_PRESSURE.sensMax ){ FR_BRAKE_PRESSURE.sensMax = FR_BRAKE_PRESSURE.sensReadVal; }
+          FR_BRAKE_PRESSURE.sensAvg += FR_BRAKE_PRESSURE.sensReadVal;
+          FR_BRAKE_PRESSURE.sensCount++;
+
+          // FL brake pressure
+          if      ( FL_BRAKE_PRESSURE.sensReadVal < FL_BRAKE_PRESSURE.sensMin ){ FL_BRAKE_PRESSURE.sensMin = FL_BRAKE_PRESSURE.sensReadVal; }
+          else if ( FL_BRAKE_PRESSURE.sensReadVal > FL_BRAKE_PRESSURE.sensMax ){ FL_BRAKE_PRESSURE.sensMax = FL_BRAKE_PRESSURE.sensReadVal; }
+          FL_BRAKE_PRESSURE.sensAvg += FL_BRAKE_PRESSURE.sensReadVal;
+          FL_BRAKE_PRESSURE.sensCount++;
+
+          // RR brake pressure
+          if      ( RR_BRAKE_PRESSURE.sensReadVal < RR_BRAKE_PRESSURE.sensMin ){ RR_BRAKE_PRESSURE.sensMin = RR_BRAKE_PRESSURE.sensReadVal; }
+          else if ( RR_BRAKE_PRESSURE.sensReadVal > RR_BRAKE_PRESSURE.sensMax ){ RR_BRAKE_PRESSURE.sensMax = RR_BRAKE_PRESSURE.sensReadVal; }
+          RR_BRAKE_PRESSURE.sensAvg += RR_BRAKE_PRESSURE.sensReadVal;
+          RR_BRAKE_PRESSURE.sensCount++;
+
+          // RL brake pressure
+          if      ( RL_BRAKE_PRESSURE.sensReadVal < RL_BRAKE_PRESSURE.sensMin ){ RL_BRAKE_PRESSURE.sensMin = RL_BRAKE_PRESSURE.sensReadVal; }
+          else if ( RL_BRAKE_PRESSURE.sensReadVal > RL_BRAKE_PRESSURE.sensMax ){ RL_BRAKE_PRESSURE.sensMax = RL_BRAKE_PRESSURE.sensReadVal; }
+          RL_BRAKE_PRESSURE.sensAvg += RL_BRAKE_PRESSURE.sensReadVal;
+          RL_BRAKE_PRESSURE.sensCount++;
+
+          // RR rotor temp
+          if      ( FR_ROTOR_TEMP.sensReadVal < FR_ROTOR_TEMP.sensMin ){ FR_ROTOR_TEMP.sensMin = FR_ROTOR_TEMP.sensReadVal; }
+          else if ( FR_ROTOR_TEMP.sensReadVal > FR_ROTOR_TEMP.sensMax ){ FR_ROTOR_TEMP.sensMax = FR_ROTOR_TEMP.sensReadVal; }
+          FR_ROTOR_TEMP.sensAvg += FR_ROTOR_TEMP.sensReadVal;
+          FR_ROTOR_TEMP.sensCount++;
+
+          // FL rotor temp
+          if      ( FL_ROTOR_TEMP.sensReadVal < FL_ROTOR_TEMP.sensMin ){ FL_ROTOR_TEMP.sensMin = FL_ROTOR_TEMP.sensReadVal; }
+          else if ( FL_ROTOR_TEMP.sensReadVal > FL_ROTOR_TEMP.sensMax ){ FL_ROTOR_TEMP.sensMax = FL_ROTOR_TEMP.sensReadVal; }
+          FL_ROTOR_TEMP.sensAvg += FL_ROTOR_TEMP.sensReadVal;
+          FL_ROTOR_TEMP.sensCount++;
+
+          // track temp
+          if      ( TRACK_TEMP.sensReadVal < TRACK_TEMP.sensMin ){ TRACK_TEMP.sensMin = TRACK_TEMP.sensReadVal; }
+          else if ( TRACK_TEMP.sensReadVal > TRACK_TEMP.sensMax ){ TRACK_TEMP.sensMax = TRACK_TEMP.sensReadVal; }
+          TRACK_TEMP.sensAvg += TRACK_TEMP.sensReadVal;
+          TRACK_TEMP.sensCount++;
+
+          // water temp between radiators
+          if      ( WATER_TEMP_BETWEEN_RADS.sensReadVal < WATER_TEMP_BETWEEN_RADS.sensMin ){ WATER_TEMP_BETWEEN_RADS.sensMin = WATER_TEMP_BETWEEN_RADS.sensReadVal; }
+          else if ( WATER_TEMP_BETWEEN_RADS.sensReadVal > WATER_TEMP_BETWEEN_RADS.sensMax ){ WATER_TEMP_BETWEEN_RADS.sensMax = WATER_TEMP_BETWEEN_RADS.sensReadVal; }
+          WATER_TEMP_BETWEEN_RADS.sensAvg += WATER_TEMP_BETWEEN_RADS.sensReadVal;
+          WATER_TEMP_BETWEEN_RADS.sensCount++;
+
+          // steering wheel angle
+          if      ( SWA.sensReadVal < SWA.sensMin ){ SWA.sensMin = SWA.sensReadVal; }
+          else if ( SWA.sensReadVal > SWA.sensMax ){ SWA.sensMax = SWA.sensReadVal; }
+          SWA.sensAvg += SWA.sensReadVal;
+          SWA.sensCount++;
+
+
           break;
         break;
       }
@@ -282,7 +394,9 @@ static void ANA_TO_SENSORVAL(int sensGroup)
       switch(sensGroup)
       {
         case 0:
-          // conversions will go here
+
+          // raw sensor input to teensy ( milivolts * 10 )
+          SWA.
           break;
       }
       break;
