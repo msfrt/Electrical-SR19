56b063ce795253df6584640a912a0fb57555025a
kopecnic <44992841+kopecnic@users.noreply.github.com>
2019-04-06 15:43:28 -0400

    Update PDM.ino enabled CAN1
    Trying to debug CAN1 to get it to work

--- Teensy Codes/PDM/PDM.ino
+++ Teensy Codes/PDM/PDM.ino
@@ -1375,7 +1375,7 @@ static void CAN_DATA_SEND(int id, int len, int busNo)
       break;
 
     case 1:
-      Can0.write(msg);  // this is send the static
+      Can1.write(msg);  // this is send the static
       break;
   }
 
