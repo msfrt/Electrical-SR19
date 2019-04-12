8d604fd83cef04935d99f1a043aca1cd1c1e3275
Dave Yonkers <daveyonks@gmail.com>
2019-04-11 20:05:19 -0400

    update testing pin

--- Teensy Codes/ATCC/ATCC.ino
+++ Teensy Codes/ATCC/ATCC.ino
@@ -144,7 +144,7 @@ void setup() {
       FL_DAMPER_POS.mV10unit    = 0; // mV*10 per sensor unit
       FL_DAMPER_POS.scaleFact   = 0.1;
 
-      TRACK_TEMP.pin         = A7;
+      TRACK_TEMP.pin         = A6;
       TRACK_TEMP.zeroMVolt10 = 400; // mV*10
       TRACK_TEMP.mV10unit    = 300.0000; // mV*10 per sensor unit
       TRACK_TEMP.scaleFact   = 0.1;
@@ -458,9 +458,9 @@ void analogToBoschTempVal( sensor &SENSOR )
 
 
   // multiply the calculated temperatures by their factors for CAN
-  sensAvg *= (1.0000 / SENSOR.scaleFact)
-  // sensMin *= (1.0000 / SENSOR.scaleFact)
-  // sensMax *= (1.0000 / SENSOR.scaleFact)
+  sensAvg *= (1.0000 / SENSOR.scaleFact);
+  // sensMin *= (1.0000 / SENSOR.scaleFact);
+  // sensMax *= (1.0000 / SENSOR.scaleFact);
 
 
 
@@ -544,7 +544,7 @@ void calculateAndLaunchCAN()
         analogToSensorVal(FR_BRAKE_PRESSURE);
         analogToSensorVal(TRACK_TEMP);
         // print the track temp for ATCC testing
-        Serial.println("Track temp CAN value: " + TRACK_TEMP.actualAvg);
+        Serial.print("Track temp CAN value: "); Serial.println(TRACK_TEMP.actualAvg);
         // put the results into a message buffer
         msg.buf[0] = messageCount100Hz; // counter
         msg.buf[1] = FL_BRAKE_PRESSURE.actualAvg;
